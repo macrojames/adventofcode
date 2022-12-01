5e0d0cca64158934f4168ab57c3dacbfc755c55d
macrojames <482151+macrojames@users.noreply.github.com>
2022-12-01 08:23:16 +0100

    Tag 1, solved

---
+++ 2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import os
+from util import read_input_lines, read_input_raw
+
+SAMPLE = True
+
+day = os.path.basename(__file__).split(".py")[0]
+_input = read_input_raw(day, SAMPLE)
+
+
---
+++ util.py
@@ -0,0 +1,13 @@
+import os
+
+def open_input(day, sample_mode):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    ext = '.sample' if sample_mode else '.input'
+    return open(os.path.join(dir_path, 'inputs', f"{day}{ext}"))
+
+
+def read_input_lines(day, sample_mode):
+    return [_.strip() for _ in open_input(day, sample_mode).readlines()]
+
+def read_input_raw(day, sample_mode):
+    return open_input(day, sample_mode).read().strip()
