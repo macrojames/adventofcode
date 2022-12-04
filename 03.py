8f537caea08b9a31d826259223442a1f54fdafb6
macrojames <482151+macrojames@users.noreply.github.co>
2022-12-04 18:46:37 +0100

    Tag 3, 0.13s

--- 03.py
+++ 03.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 import os
 import time
+import string
 from util import read_input_lines, read_input_raw
 start_timer = time.time()
 
-SAMPLE = True
+SAMPLE = False
 
 day = os.path.basename(__file__).split(".py")[0]
 _input = read_input_lines(day, SAMPLE)
 
-print("Part 1: ", )
-print("Part 2: ", )
+sum1, sum2 = 0, 0 
+def get_prio(letter):
+    return (string.ascii_lowercase + string.ascii_uppercase).index(letter) + 1
+
+for i in range(len(_input) // 3):
+    group = _input[i*3:i*3+3]
+    for rucksack in group:
+        left = rucksack[:len(rucksack)//2]
+        right = rucksack[len(rucksack)//2:]
+        
+        double = set(left) & set(right)
+        sum1 += sum(map(get_prio, double))
+        print("Debug:", double)
+    
+    badge = set(group[0]) & set(group[1]) & set(group[2])
+    sum2 += sum(map(get_prio, badge))
+    print(group, badge)
+
+print("Part 1: ", sum1)
+print("Part 2: ", sum2)
 print(f"Time elapsed: {round(time.time() - start_timer, 3)}s")
