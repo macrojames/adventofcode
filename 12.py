773b7d0a903ba47a3f289aa4f51b1e1e64f6f371
macrojames <482151+macrojames@users.noreply.github.com>
2022-12-12 22:52:29 +0100

    Tag 12, 1.2s/100% Korrektheit, 0.092s vermutlich auch korrekt :D

--- 12.py
+++ 12.py
@@ -1,14 +1,55 @@
 #!/usr/bin/env python3
 import os
 import time
-from util import read_input_lines, read_input_raw
+from util import dijkstra, read_input_lines, read_input_raw, arr2d_get_idx, arr2d_get_near_idx, arr2d_get_all_idx
+from queue import PriorityQueue
 start_timer = time.time()
 
-SAMPLE = True
+SAMPLE = False
 
 day = os.path.basename(__file__).split(".py")[0]
 _input = read_input_lines(day, SAMPLE)
+grid = [list(_) for _ in _input]
+start = arr2d_get_idx(grid, "S")
+end = arr2d_get_idx(grid, "E")
+all_a = arr2d_get_all_idx(grid, "a") + [start]
 
-print("Part 1: ", )
-print("Part 2: ", )
+def get_cost(src, dst):
+    ord_src = ord(src) if src.islower() else ord('a')    # Start always == a
+    ord_dst = ord(dst) if dst.islower() else ord('z')    # End: doesn't matter, no effort
+    cost = 1 if (ord_dst - ord_src) <= 1 else 10E9
+    return cost
+
+def get_cost2(src, dst):
+    if dst == 'a':    # bricht ab bei falschem Ziel
+        return float('inf')
+    else:
+        return get_cost(src, dst)
+def get_cost3(src, dst):
+    if dst == 'a':
+        return 0
+    else:
+        return get_cost(src, dst)
+
+
+dist, previous = dijkstra(grid, start, end, get_cost)
+print("Part 1:", dist[end])
+
+dist, previous = dijkstra(grid, start, end, get_cost3)
+node = previous[end]
+path = [node]
+while grid[node[0]][node[1]] != 'a':
+   y,x = previous.get(node)
+   
+   info = [y,x, grid[y][x]]
+   path.append(info)
+   node = previous[node]
+print("Part 2a:", len(path))
+
+#results = []
+#for a in all_a:
+#    dist, previous = dijkstra(grid, a, end, get_cost2)
+#    if end in dist:
+#        results.append((a, dist[end]))
+#print("Part 2: ", sorted(results, key=lambda x:x[1])[0][1])
 print(f"Time elapsed: {round(time.time() - start_timer, 3)}s")
--- util.py
+++ util.py
@@ -1,4 +1,7 @@
 import os
+from queue import PriorityQueue
+import sys
+
 
 def open_input(day, sample_mode):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -9,5 +12,78 @@ def open_input(day, sample_mode):
 def read_input_lines(day, sample_mode):
     return [_.strip() for _ in open_input(day, sample_mode).readlines()]
 
+
 def read_input_raw(day, sample_mode):
     return open_input(day, sample_mode).read()
+
+
+def arr2d_get_near_idx(arr2d: list[list], y: int, x: int) -> list[tuple]:
+    # Return neighbor indexes
+    nt = []
+    if x >= 1:
+        nt.append((y, x-1))
+    if x + 1 < len(arr2d[y]):
+        nt.append((y, x+1))
+    if y >= 1:
+        nt.append((y-1, x))
+    if y + 1 < len(arr2d):
+        nt.append((y+1, x))
+    return nt
+
+
+def arr2d_get_neighbors(arr2d: list[list], y: int, x: int):
+    # Return neighbor values
+    return [arr2d[y][x] for y, x in arr2d_get_near_idx(arr2d, x, y)]
+
+
+def arr2d_get_idx(arr2d: list[list], value: any) -> tuple:
+    """ Returns first occurence coordinates of value in arr2d
+        Raises ValueError if no match is found
+    """
+    for y, row in enumerate(arr2d):
+        if value in row:
+            return y, row.index(value)
+    raise ValueError
+
+
+def arr2d_get_all_idx(arr2d: list[list], value: any) -> list[tuple]:
+    """ Returns all occurence coordinates of value in arr2d
+        Raises ValueError if no match is found
+    """
+    ret = []
+    for y, row in enumerate(arr2d):
+        if value in row:
+            ret.append((y, row.index(value)))
+    if not ret:
+        raise ValueError
+    return ret
+
+
+def dijkstra(grid, start, end, get_cost):
+    nodes_open = PriorityQueue()
+    nodes_open.put((0, start))
+    dist = {start: 0}
+    previous = {}
+    visited = set()
+
+    while nodes_open:
+        while not nodes_open.empty():
+            _, node = nodes_open.get()
+            if node not in visited:
+                break
+        else:
+            break
+        visited.add(node)
+        if node == end:
+            break
+        check_neighbors = [_ for _ in arr2d_get_near_idx(grid, *node) if _ not in visited]
+        for neighbor in check_neighbors:
+            new_dist = dist.get(node) + get_cost(grid[node[0]][node[1]], grid[neighbor[0]][neighbor[1]])
+            if new_dist == float('inf'):    # Abbruchmöglichkeit durch cost implementierung
+                next
+            if new_dist < dist.get(neighbor, float('inf')):
+                nodes_open.put((new_dist, neighbor))
+                dist[neighbor] = new_dist
+                previous[neighbor] = node
+                
+    return dist, previous
