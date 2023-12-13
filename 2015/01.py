44fedc1c7853ea94769ae0a7a79f1d7c649f9cc9
macrojames <482151+macrojames@users.noreply.github.com>
2023-12-13 21:46:17 +0100

    Tooling

--- 2015/01.py
+++ 2015/01.py
@@ -4,16 +4,18 @@ import os.path
 from util import read_input_lines
 
 start_timer = time.time()
-SAMPLE = True
+
+SAMPLE = False
 DEBUG = True
-inputs = read_input_lines(os.path.splitext(os.path.basename(__file__))[0], SAMPLE)
+
+inputs = read_input_lines(os.path.splitext(os.path.basename(__file__))[0], SAMPLE)[0]
 
 
 def part1():
-    return 1
+    return inputs.count("(") - inputs.count(")")
 
 
-#print("Part 1: ", part1())
+print("Part 1: ", part1())
 #print("Part 2: ", part2())
 
 print(f"Time elapsed: {round(time.time() - start_timer, 3)}s")
