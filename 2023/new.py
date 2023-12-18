2ed959aaf23c4c8f16e88958bbd355fcf6e9cc22
macrojames <482151+macrojames@users.noreply.github.com>
2023-12-18 11:33:58 +0100

    New Template

--- 2023/new.py
+++ 2023/new.py
@@ -2,12 +2,14 @@
 import time
 import os.path
 from util import read_input_lines
-
+import colorama
+colorama.init()
 start_timer = time.time()
-SAMPLE = True
+SAMPLE = os.environ.get("SAMPLE", "True") == "True"
 DEBUG = True
 inputs = read_input_lines(os.path.splitext(os.path.basename(__file__))[0], SAMPLE)
-
+if SAMPLE: print(colorama.Fore.RED + "SAMPLE MODE")
+else: print(colorama.Fore.GREEN + "SOLUTION MODE")
 
 def part1():
     return 1
@@ -15,4 +17,5 @@ def part1():
 print("Part 1: ", part1())
 #print("Part 2: ", part2())
 
-print(f"Time elapsed: {round(time.time() - start_timer, 3)}s")
+print(f"{colorama.Fore.BLUE}Time elapsed: {round(time.time() - start_timer, 3)}s")
+
