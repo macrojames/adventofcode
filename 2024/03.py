65f8d0f2c013f0339b18c2ab9807c0ce8f77cf51
macrojames <482151+macrojames@users.noreply.github.com>
2024-12-03 08:05:27 +0100

    regex übung

--- 2024/03.py
+++ 2024/03.py
@@ -3,6 +3,7 @@ import time
 import os.path
 from util import read_input_lines
 import colorama
+import re
 colorama.init()
 start_timer = time.time()
 SAMPLE = os.environ.get("SAMPLE", "True") == "True"
@@ -12,10 +13,29 @@ if SAMPLE: print(colorama.Fore.RED + "SAMPLE MODE")
 else: print(colorama.Fore.GREEN + "SOLUTION MODE")
 
 def part1():
-    return 1
+    text = "".join(inputs)
+    groups = re.findall("mul\\((\d{1,3}),(\d{1,3})\\)", text)
+    result = 0
+    for l, r in groups:
+        result += int(l) * int(r)
+    return result
+
+
+def part2():
+    mode = 'do()'
+    text = "".join(inputs)
+    groups = re.findall("(don't\\(\\)|do\\(\\))|mul\\((\d{1,3}),(\d{1,3})\\)", text)
+    result = 0
+    for cmd, l, r in groups:
+        if cmd:
+            mode = cmd
+            continue
+        if mode == 'do()':
+            result += int(l) * int(r)
+    return result
 
 print("Part 1: ", part1())
-#print("Part 2: ", part2())
+print("Part 2: ", part2())
 
 print(f"{colorama.Fore.BLUE}Time elapsed: {round(time.time() - start_timer, 3)}s")
 
