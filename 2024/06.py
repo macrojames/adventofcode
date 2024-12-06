5ea89106cea5a0d7fc75d0307ea9c62f65bcf18b
macrojames <482151+macrojames@users.noreply.github.com>
2024-12-06 09:30:42 +0100

    6.1

--- 2024/06.py
+++ 2024/06.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import time
 import os.path
-from util import read_input_lines
+from util import read_input_lines, get_indices
 import colorama
 colorama.init()
 start_timer = time.time()
@@ -11,11 +11,35 @@ inputs = read_input_lines(os.path.splitext(os.path.basename(__file__))[0], SAMPL
 if SAMPLE: print(colorama.Fore.RED + "SAMPLE MODE")
 else: print(colorama.Fore.GREEN + "SOLUTION MODE")
 
+dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+blocks = set()
+
+for y, row in enumerate(inputs):
+    for column in get_indices(row, "#"):
+        blocks.add((y, column))
+    if (column := row.find("^")) > 0:
+        start = (y, column)
+
 def part1():
-    return 1
+    visited = set()
+    facing = 0
+    r, c = start
+    while 0 <= r < len(inputs) and 0 <= c < len(inputs[0]):
+        visited.add((r, c))
+        
+        nr, nc = r + dirs[facing][0], c + dirs[facing][1]
+        if (nr, nc) in blocks:
+            facing = (facing + 1) % 4
+            nr, nc = r + dirs[facing][0], c + dirs[facing][1]
+        else:
+            r, c = nr, nc
+
+    return len(visited)
+
+
 
 print("Part 1: ", part1())
-#print("Part 2: ", part2())
+print("Part 2: ", part2())
 
 print(f"{colorama.Fore.BLUE}Time elapsed: {round(time.time() - start_timer, 3)}s")
 
