8c604d59b29febb798f878aa796c58f840d33b30
macrojames <482151+macrojames@users.noreply.github.com>
2024-12-08 10:24:47 +0100

    Tag 8, ab jetzt ohne inputs

--- 2024/08.py
+++ 2024/08.py
@@ -2,6 +2,8 @@
 import time
 import os.path
 from util import read_input_lines
+from collections import defaultdict
+from itertools import combinations, permutations
 import colorama
 colorama.init()
 start_timer = time.time()
@@ -11,11 +13,40 @@ inputs = read_input_lines(os.path.splitext(os.path.basename(__file__))[0], SAMPL
 if SAMPLE: print(colorama.Fore.RED + "SAMPLE MODE")
 else: print(colorama.Fore.GREEN + "SOLUTION MODE")
 
-def part1():
-    return 1
+antennas = defaultdict(set)
+max_r = len(inputs) - 1
+max_c = len(inputs[0]) - 1
 
-print("Part 1: ", part1())
-#print("Part 2: ", part2())
+for r, row in enumerate(inputs):
+    for c, letter in enumerate(row):
+        if letter == '.': continue
+        antennas[letter].add((r, c))
+
+def part1():
+    antinodes = set()
+    for letter, nodes in antennas.items():
+        for (r1, c1), (r2, c2) in permutations(nodes, 2):  #combinations -> mirror up und down. permutations, vertauscht die deltas?!
+            dr, dc = r1 - r2, c1 - c2
+            anr, anc = (r1 + dr, c1 + dc)
+            if 0 <= anr <= max_r and 0 <= anc <= max_c:
+                antinodes.add((anr, anc))
+    return antinodes
+
+def part2():
+    antinodes = set()
+    for letter, nodes in antennas.items():
+        for (r1, c1), (r2, c2) in permutations(nodes, 2):  #combinations -> mirror up und down. permutations, vertauscht die deltas?!
+            dr, dc = r1 - r2, c1 - c2
+            for i in range(1000):
+                anr, anc = (r1 + i*dr, c1 + i*dc)
+                if 0 <= anr <= max_r and 0 <= anc <= max_c:
+                    antinodes.add((anr, anc))
+                else:
+                    break
+    return antinodes
+
+print("Part 1: ", len(part1()))
+print("Part 1: ", len(part2()))
 
 print(f"{colorama.Fore.BLUE}Time elapsed: {round(time.time() - start_timer, 3)}s")
 
