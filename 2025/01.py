0e4a3faf7b3ed9b5466927dd0025bf963ce0e2cc
macrojames <482151+macrojames@users.noreply.github.com>
2025-12-01 10:28:16 +0100

    Tag 1

--- 2025/01.py
+++ 2025/01.py
@@ -19,11 +19,32 @@ def part1():
         num = int(instruction[1:])
         if d == 'L': current -= num
         elif d == 'R': current += num
+        current %= 100
         if current == 0: counter += 1
     return counter
 
+def part2():
+    current = 50
+    counter = 0
+    for instruction in inputs:
+        d = instruction[0]
+        num = int(instruction[1:])
+        old = current
+        if d == 'L': current -= num
+        elif d == 'R': current += num
+
+        print(f"{old} -> {current} by {d}{num}")
+        print(f"{abs(current) // 100} {current % 100} => ", end="")
+        steps = abs(current) // 100
+        if old > 0 and current <=0:
+            steps += 1
+        counter += steps
+        current %= 100
+        print(f" +{steps} = {counter}")
+    return counter
+
 print("Part 1: ", part1())
-#print("Part 2: ", part2())
+print("Part 2: ", part2())
 
 print(f"{colorama.Fore.BLUE}Time elapsed: {round(time.time() - start_timer, 3)}s")
 
