0231e649a2ec273e5e96633b4a3249997fbe1a00
macrojames <482151+macrojames@users.noreply.github.com>
2025-12-03 08:58:33 +0100

    Tag 3

--- 2025/03.py
+++ 2025/03.py
@@ -12,10 +12,30 @@ if SAMPLE: print(colorama.Fore.RED + "SAMPLE MODE")
 else: print(colorama.Fore.GREEN + "SOLUTION MODE")
 
 def part1():
-    return 1
+    sum = 0
+    for line in inputs:
+        d1, d2 = 0, 0
+        l, r = 0, len(line)
+        d1 = max(line[l: r-1])
+        l = line.find(d1) + 1
+        d2 = max(line[l: r])
+        sum += (10* int(d1)) + int(d2)
+    return sum
+
+def part2():
+    sum = 0
+    for line in inputs:
+        digits = []
+        l, r = 0, len(line)
+        for i in range(12):
+            digits.append(max(line[l: r-(11-i)]))
+            l = line.find(digits[-1], l) + 1
+        num = "".join(digits)
+        sum += int(num)
+    return sum
 
 print("Part 1: ", part1())
-#print("Part 2: ", part2())
+print("Part 2: ", part2())
 
 print(f"{colorama.Fore.BLUE}Time elapsed: {round(time.time() - start_timer, 3)}s")
 
